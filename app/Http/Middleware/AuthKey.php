027733171d47494fe6336b1e684dfd660f5ed629
stormfireuttam <uttam29mittal@gmail.com>
2021-02-17 10:54:31 +0530

    Token Authentication with Middleware

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('APP_KEY');
+        if($token != 'ABCDEFGHIJK') {
+            return response()->json([
+                'message' => 'App Key Not Found',
+                ],401);                 //   401: Unauthorized
+        }
+        return $next($request);
+    }
+}
